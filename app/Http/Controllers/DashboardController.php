45ae40ee5005b5fd6b0772f67b16a4f25ec8b146
nabila <e41230480@student.polije.ac.id>
2025-03-14 10:03:54 +0700

    Acara 8

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,12 +1,1 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class DashboardController extends Controller
-{
-    public function index(){
-        return view('backend.dashboard');
-    }
-}
+ac
